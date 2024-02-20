4881aba7593042b3c7f32d99de65bc3d059e366b
Elena Bozhenkova <bozhenkova-elena@yandex.com>
2024-02-20 12:55:44 +0300

    Добавить HashMaps для хранения всех типов задач
    Задачи, эпики и подзадачи будут храниться в трех разных таблицах,где ключом будет id, значением - объект

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,2 +1,7 @@
+import java.util.HashMap;
+
 public class TaskManager {
+    protected HashMap<Integer,Task> tasks;
+    protected HashMap<Integer,Epic> epics;
+    protected HashMap<Integer,SubTask> subTasks;
 }
