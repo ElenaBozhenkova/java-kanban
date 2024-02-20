d4c93ce36c38420ba70101b4b52edfbf2361a8f6
Elena Bozhenkova <bozhenkova-elena@yandex.com>
2024-02-20 11:04:10 +0300

    Добавить новый родительский класс Task
    Этот класс обеспечивает создание простой задачи для канбан-доски, хранит поля id,название,описание,статус задачи, а также конструктор для создания новых объектов задач.

---
+++ src/Task.java
@@ -0,0 +1,10 @@
+public class Task {
+    protected int id;
+    protected String taskName;
+    protected String taskDescription;
+    protected TaskStatus taskStatus;
+    protected TaskType taskType;
+
+
+
+}
