9a72876b2e7f8f53c572a208e9c482355ff6440b
Elena Bozhenkova <bozhenkova-elena@yandex.com>
2024-02-20 12:58:44 +0300

    Проинициализировать таблицы в конструкторе

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -4,4 +4,10 @@ public class TaskManager {
     protected HashMap<Integer,Task> tasks;
     protected HashMap<Integer,Epic> epics;
     protected HashMap<Integer,SubTask> subTasks;
+
+    public TaskManager() {
+        this.tasks = new HashMap<>();
+        this.epics = new HashMap<>();
+        this.subTasks = new HashMap<>();
+    }
 }
