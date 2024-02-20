8ec11c268246087db382eb621669b07f46c62961
Elena Bozhenkova <bozhenkova-elena@yandex.com>
2024-02-20 12:39:22 +0300

    Переопределить конструкторы класса SubTask

--- src/SubTask.java
+++ src/SubTask.java
@@ -1,4 +1,17 @@
 public class SubTask extends Task {
     protected int epicId;
 
+    SubTask(String taskName, String taskDescription) {
+        super(taskName,taskDescription);
+        this.taskType = TaskType.SUBTASK;
+        this.taskStatus = TaskStatus.NEW;
+    }
+
+    SubTask(Integer id,String taskName, String taskDescription,TaskStatus taskStatus) {
+        super(id, taskName, taskDescription, taskStatus);
+        this.taskType = TaskType.SUBTASK;
+        this.id = id;
+        this.taskStatus = taskStatus;
+    }
+
 }
