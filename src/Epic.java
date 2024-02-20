cd16de21e3b16de098b3a490f9f3b18da15b484b
Elena Bozhenkova <bozhenkova-elena@yandex.com>
2024-02-20 12:19:51 +0300

    Переопределить конструкторы класса Epic

--- src/Epic.java
+++ src/Epic.java
@@ -3,6 +3,19 @@ import java.util.ArrayList;
 public class Epic extends Task {
     private ArrayList<SubTask> epicSubTasks;
 
+    Epic(String taskName, String taskDescription) {
+        super(taskName,taskDescription);
+        epicSubTasks = new ArrayList<>();
+        this.taskType = TaskType.EPIC;
+        this.taskStatus = TaskStatus.NEW;
+    }
 
+    Epic(Integer id,String taskName, String taskDescription, TaskStatus taskStatus) {
+        super(id,taskName,taskDescription,taskStatus);
+        this.taskType = TaskType.EPIC;
+        epicSubTasks = new ArrayList<>();
+        this.id = id;
+        this.taskStatus = taskStatus;
+    }
 
 }
