db3076fa46e93101c2e2dfc97283ad5b0e8f9a4d
Elena Bozhenkova <bozhenkova-elena@yandex.com>
2024-02-20 11:31:06 +0300

    Добавить сеттеры и геттеры для всех переменных класса Task

--- src/Task.java
+++ src/Task.java
@@ -19,7 +19,43 @@ public class Task {
         this.taskStatus = taskStatus;
         this.taskType = TaskType.TASK;
     }
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getTaskName() {
+        return taskName;
+    }
 
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public TaskStatus getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(TaskStatus taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public TaskType getTaskType() {
+        return taskType;
+    }
+
+    public void setTaskType(TaskType taskType) {
+        this.taskType = taskType;
+    }
 }
