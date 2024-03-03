63c8c20ca00fbfda4fb89b6e3fff03b8df7c24a4
Elena Bozhenkova <bozhenkova-elena@yandex.com>
2024-03-03 20:55:38 +0300

    Добавить класс реализовывающий интерфейс HistoryManager

---
+++ src/InMemoryHistoryManager.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryHistoryManager implements HistoryManager {
+    private List<Task> history = new ArrayList<>();
+
+    @Override
+    public void add(Task task) {
+        int memorySize = 10;
+        if (history.size() < memorySize) {
+            history.add(task);
+        }
+        else {
+            history.removeFirst();
+            history.add(task);
+        }
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return new ArrayList<>(history);
+    }
+}
